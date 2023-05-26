619bc94f4abff4b748721c8089914fd28caec6a2
Neyvit <successfullproject@gmail.com>
2023-05-26 18:56:05 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/examgenerator/ExamGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamGeneratorApplication.class, args);
+    }
+
+}
